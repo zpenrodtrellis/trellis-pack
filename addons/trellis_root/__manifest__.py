2a60a773bd29b06bffe7a3dee0a667dd176c6da4
Zach Penrod <zpenrod@trellis.systems>
2025-09-28 13:38:42 -0400

    Fix load order: access rights after views

--- addons/trellis_root/__manifest__.py
+++ addons/trellis_root/__manifest__.py
@@ -4,9 +4,9 @@
     "author": "Trellis",
     "depends": ["base", "mrp", "calendar"],
     "data": [
-        "security/ir.model.access.csv",
         "views/root_menu.xml",
         "views/schedule_view.xml",
+        "security/ir.model.access.csv",  # load after model exists
     ],
     "installable": True,
     "application": True,
