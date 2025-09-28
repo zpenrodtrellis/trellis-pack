119da9c95fd31179d7c0c8d32c5f112b38a1edb0
Zach Penrod <zpenrod@trellis.systems>
2025-09-28 13:57:54 -0400

    Fix load order: ensure model exists before views

--- addons/trellis_root/__manifest__.py
+++ addons/trellis_root/__manifest__.py
@@ -2,10 +2,11 @@
     "name": "Trellis Root",
     "version": "19.0.1.0.0",
     "author": "Trellis",
+    "license": "LGPL-3",
     "depends": ["base"],
     "data": [
-        "views/schedule_view.xml",
         "security/ir.model.access.csv",
+        "views/schedule_view.xml",
     ],
     "installable": True,
     "application": True,
