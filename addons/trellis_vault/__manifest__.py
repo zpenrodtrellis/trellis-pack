3655b705a69221bf66f238f934eeea0a8d0ab215
Zach Penrod <zpenrod@trellis.systems>
2025-10-07 22:41:18 -0400

    Remove redundant chatter view ref to reload form cleanly

--- addons/trellis_vault/__manifest__.py
+++ addons/trellis_vault/__manifest__.py
@@ -4,13 +4,12 @@
     "summary": "Vault master data + controlled documents + training records",
     "author": "Trellis Systems",
     "license": "LGPL-3",
-    "depends": ["base", "mail"],  # <-- keep mail, chatter needs it
+    "depends": ["base", "mail"],
     "data": [
         "security/ir.model.access.csv",
         "views/strain_views.xml",
         "views/mother_tag_views.xml",
         "views/controlled_document_views.xml",
-        "views/controlled_document_chatter.xml",  # <-- add this line
         "views/training_record_views.xml",
         "views/menus.xml",
     ],
